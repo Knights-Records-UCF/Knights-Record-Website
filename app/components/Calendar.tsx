067c0f3c30b4342107493f0d40c44bf5842cd91e
Carlos <lopez212318@gmail.com>
2026-03-12 21:38:06 -0400

    Highlight current day of the month + setup eventsPerDay object
    Created an object that consists of arrays of calendar event objects per day. This will be used to create a modal that displays the full list of events per day if there's more than 3 events for that day.

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,6 +1,30 @@
 "use client";
 import { useEffect, useState } from "react";
 
+
+
+type CalendarEvent = {
+    id: string;
+    summary: string;
+    description: string;
+
+    // add the time 
+    start: {
+        dateTime: string;
+        date: string;
+    }
+    end: {
+        dateTime: string;
+        date: string;
+    }
+
+}
+
+
+type CalendarResponse = {
+    items: CalendarEvent[];
+}
+
 const months = [
     "January", // 0 
     "February",
@@ -31,6 +55,8 @@ export default function Calendar() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
     const [events, setEvents] = useState<CalendarEvent[]>([]);
+    const [showModal, setShowModal] = useState(false);
+
 
     useEffect(() => {
         async function fetchCalendar() {
@@ -51,15 +77,6 @@ export default function Calendar() {
 
                 // console.log("single event", data.items[0]);
 
-                const GClength = data.items.length;
-
-                console.log(GClength); // 5
-
-                console.log("++++++++++++++++++++++++++++++++++++++");
-                dateTesting(GClength, data, currDate);
-
-
-
                 // 
 
                 // dateTesting(GClength, data, currDate);
@@ -78,67 +95,38 @@ export default function Calendar() {
 
     }, []);
 
-    // Just outputs the events, and whether they are in the past or future compared to the current date
-    function dateTesting(length: number, data: CalendarResponse, currDate: Date) {
-        for (let i = 0; i < length; i++) {
-            let GCDate: Date;
-            let GCDateDay: number;
-            // output date and datetime
-            // Date corresponds to a full day event
-            // Datetime corresponds to the specific time
-            if (data.items[i].start.dateTime) {
-
-                console.log(`Datetime of event ${i} which is ${data.items[i].summary}: ${data.items[i].start.dateTime}`);
 
 
-                GCDate = new Date(data.items[i].start.dateTime);
-                console.log(`DateTime object: ${GCDate}`);
 
-                GCDateDay = GCDate.getDate();
-                console.log(`The day of the month of the event, ${data.items[i].summary} is ${GCDateDay}`);
-
-            }
-            else {
-
-                console.log(`This is a full day event (${i}) "${data.items[i].summary}" and the date is ${data.items[i].start.date}`);
-
-                GCDate = new Date(data.items[i].start.date);
-                console.log(`Date object: ${GCDate}`);
-
-
-                GCDateDay = GCDate.getDate();
-                console.log(`The day of the month of the event, ${data.items[i].summary} is ${GCDateDay}`);
-            }
-
-            console.log(`${GCDate > currDate}`); // Outputs true if date is in future, otherwise false
-
-        }
-    }
-
-
-    console.log("--------------------------------------");
-    const currDate = new Date( ); // Current date and time
+    const currDate = new Date(); // Current date and time
     const currMonth = currDate.getMonth(); // 0-11
-    console.log(`The current date is: ${currDate}`)
-    console.log(`The month is ${months[currMonth]}`);
+    // console.log(`The current date is: ${currDate}`)
+    // console.log(`The month is ${months[currMonth]}`);
 
     const lastOfMonth = new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0);
 
-    console.log(`The last day of the month of ${months[currDate.getMonth()]} is ${lastOfMonth}`);
-    console.log(`The index of the last weekday of the month is: ${lastOfMonth.getDay()} and that weekday is ${weekDays[lastOfMonth.getDay()]}`);
+    // console.log(`The last day of the month of ${months[currDate.getMonth()]} is ${lastOfMonth}`);
+    // console.log(`The index of the last weekday of the month is: ${lastOfMonth.getDay()} and that weekday is ${weekDays[lastOfMonth.getDay()]}`);
 
 
     // This is the first day of april
     const firstOfMonth = new Date(currDate.getFullYear(), currDate.getMonth(), 1);
 
-    console.log(`The first day of the month of ${months[currMonth]} is ${firstOfMonth}`);
-    console.log(`The index of the first weekday of the month is ${firstOfMonth.getDay()} and that weekday is ${weekDays[firstOfMonth.getDay()]}`);
+    // console.log(`The first day of the month of ${months[currMonth]} is ${firstOfMonth}`);
+    // console.log(`The index of the first weekday of the month is ${firstOfMonth.getDay()} and that weekday is ${weekDays[firstOfMonth.getDay()]}`);
 
     const totalDays = lastOfMonth.getDate();
-    console.log(`For the month of ${months[currDate.getMonth()]}, there are a total of ${totalDays} days`);
+    // console.log(`For the month of ${months[currDate.getMonth()]}, there are a total of ${totalDays} days`);
 
 
-
+    function Modal() {
+        return (
+            <div>
+                <h1> Modal </h1>
+                <p> hi </p>
+            </div>
+        )
+    }
 
     // For creating the calendar 
     const calendarCells = []
@@ -154,7 +142,30 @@ export default function Calendar() {
 
     console.log(`${calendarCells}`);
 
-    
+    // Creating an object where key is a number (day) and value is array of CalendarEvent objects
+    const eventsPerDay: Record<number, CalendarEvent[]> = {};
+
+    events.forEach((event) => {
+        let eventDate : Date;
+        
+        // 24-hour event vs specified event length
+        if (event.start.dateTime) {
+            eventDate = new Date(event.start.dateTime);
+        } else {
+            eventDate = new Date(event.start.date);
+        }
+
+        const day = eventDate.getDate();
+
+        // If this day doesn't exist yet in the object then initialize it with an empty array
+        if (!eventsPerDay[day]) {
+            eventsPerDay[day] = [];
+        }
+
+        // At that day, push the event object to the array
+        eventsPerDay[day].push(event);
+    })
+
 
 
     return (
@@ -168,13 +179,31 @@ export default function Calendar() {
                     {weekDays.map((day) => (
                         <h2 key={day} className="text-lg ml-2 text-[#858585]">{day}</h2>
                     ))}
-                    {calendarCells.map((day) => {
-                        // if (day == new Date().getDay())
+
+                    {calendarCells.map((day, index) => {
+                        if (day === null) {
+                            return null;
+                        }
+
+                        const dayEvents = eventsPerDay[day] ?? []; // Returns either an event of 
+                        const eventCount = dayEvents.length;
+                        console.log(`On day ${day} there are a total of ${eventCount} events`)
+                        
                         return (
                             <div className="border-t border-[#f2f2f7] border-b w-32 h-32" key={day}>
-                                <p className="ml-3 mt-2 ">{day} </p>
+                                {day === new Date().getDate() ? ( // Circle current day (someone double check if this is properly aligned please!)
+                                    <div className="w-7.5 h-7.5 bg-red-500 rounded-full ml-3 pl-1.25 pt-0.5 mt-1.5">
+                                        <p className=" ">
+                                            {day}
+                                        </p>
+                                    </div>
+                                ) : (
+                                    <p className="ml-3 mt-2">{day}</p>
+                                )}
+
                                 {events.map((event) => {
                                     let eventDate: Date;
+
                                     if (event.start.dateTime) {
                                         eventDate = new Date(event.start.dateTime);
                                     } else {
@@ -183,30 +212,31 @@ export default function Calendar() {
 
                                     let time = eventDate.toLocaleTimeString("en-US");
                                     let TOD = time.slice(-2); // Time of day (AM/PM)
-                                    console.log(`The time is currently ${time}`);
-                                    console.log(`${time.length} is the length of the time string`);
+                                    // console.log(`The time is currently ${time}`);
+                                    // console.log(`${time.length} is the length of the time string`);
 
                                     if (time.length == 10) { // 9:30:00 PM
-                                        console.log(`Time is: ${time.slice(0, 4)}`);
-                                        console.log(TOD);
-                                        console.log(`Formatted time is: ${time.slice(0,4)} ${TOD}`);
-                                        time = time.slice(0,4);
+                                        // console.log(`Time is: ${time.slice(0, 4)}`);
+                                        // console.log(TOD);
+                                        // console.log(`Formatted time is: ${time.slice(0,4)} ${TOD}`);
+                                        time = time.slice(0, 4);
                                     }
                                     else { // 10:30:00 PM
-                                        console.log(`Time is: ${time.slice(0,5)}`);
-                                        console.log(TOD);
-                                        console.log(`Formatted time is: ${time.slice(0,5)} ${TOD}`)
-                                        time = time.slice(0,5);
+                                        // console.log(`Time is: ${time.slice(0,5)}`);
+                                        // console.log(TOD);
+                                        // console.log(`Formatted time is: ${time.slice(0,5)} ${TOD}`)
+                                        time = time.slice(0, 5);
                                     }
-                                    
+
 
                                     if (eventDate.getDate() === day) {
                                         return (
-                                            <button 
+                                            <button
                                                 className="flex flex-row items-center mt-1 w-32 group focus:bg-[#E18181] rounded-sm h-4"
+                                                // onClick={() => setShowModal(true)}
                                             >
-                                                 <div className="ml-1 w-2 h-2 shrink-0 bg-[#E18181] group-focus:bg-white rounded-full "/>
-                                                 <p className="text-left pl-1 text-xs line-clamp-1">{event.summary} <span className="text-[9px] text-[#858585] group-focus:text-black ml-6">{time} {TOD}</span></p>
+                                                <div className="ml-1 w-2 h-2 shrink-0 bg-[#E18181] group-focus:bg-white rounded-full " />
+                                                <p className="text-left pl-1 text-xs line-clamp-1">{event.summary} <span className="text-[9px] text-[#858585] group-focus:text-black ml-6">{time} {TOD}</span></p>
                                             </button>
                                         );
                                     }
