d90024b250d48c09c8a79888527c58269ff1e4f2
Carlos <lopez212318@gmail.com>
2026-03-15 19:42:55 -0400

    Vinyl hover functionality

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,18 +1,5 @@
 import Vinyl from "../components/Vinyl";
 
-interface Record {
-    sleeveColor: number;
-    vinylColor: number;
-    
-}
-
-const recordArr: Record[] = [
-    {
-        sleeveColor: 0,
-        vinylColor: 0,
-    },
-    
-]
 
 export default function Artists() {
     return (
--- app/components/Vinyl.tsx
+++ app/components/Vinyl.tsx
@@ -1,42 +1,57 @@
+// When the time comes swap "sleeveColor" with "sleeveImage". We'll probably do something similar for "recordcolor"
+
 interface Record {
     sleeveColor: string;
-    vinylColor: number;
-    
+    recordColor: string;
+
 }
 
 const recordArr: Record[] = [
     {
         sleeveColor: "bg-[#81E1CE]",
-        vinylColor: 0,
+        recordColor: "bg-[#D9D9D9]"
     },
     {
         sleeveColor: "bg-[#DE81E1]",
-        vinylColor: 1,
+        recordColor: "bg-[#D9D9D9]",
     },
     {
         sleeveColor: "bg-[#E18181]",
-        vinylColor: 2,
+        recordColor: "bg-[#D9D9D9]",
     },
     {
         sleeveColor: "bg-[#619190]",
-        vinylColor: 3,
+        recordColor: "bg-[#D9D9D9]",
     },
     {
         sleeveColor: "bg-[#E1C381]",
-        vinylColor: 4,
+        recordColor: "bg-[#D9D9D9]",
     },
 ]
 
 export default function Vinyl() {
     return (
-        <div className="flex flex-col gap-4 ">
-            {recordArr.map((vinyl, i) => 
+        // Maybe switch to aside later since this is lowk like a navbar but for artists
+        <div className="flex flex-col gap-4">
+            {recordArr.map((vinyl, i) => (
                 <div
-                    key={i} 
-                    className={` w-32 h-32 ${vinyl.sleeveColor} drop-shadow-2xl shadow-md`}
-                />
-            
-            )}
+                    key={i}
+                    className="relative w-32 h-32 group"
+                >
+                    
+
+                    {/* Album Sleeve */}
+                    <div
+                        className={`absolute inset-0 z-10 ${vinyl.sleeveColor} drop-shadow-md drop-shadow-gray-400`}
+                    />
+
+                    {/* Record */}
+                    <div
+                        className={`absolute inset-0 z-0 ${vinyl.recordColor} rounded-full scale-95  transition-transform drop-shadow-gray-400 drop-shadow-md duration-300 group-hover:translate-x-16`}
+                    >
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
